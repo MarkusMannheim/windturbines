15413aa0573496b3c346f1bba0efec60896fbbd4
Markus Mannheim <markus@mannheim.media>
2023-12-05 16:46:37 +1100

    update

---
+++ resources/script.js
@@ -0,0 +1,20 @@
+document.getElementById('distanceForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    const observerHeight = parseFloat(document.getElementById('observerHeight').value);
+    const turbineHeight = parseFloat(document.getElementById('turbineHeight').value);
+  
+    if (!isNaN(observerHeight) && !isNaN(turbineHeight)) {
+      const distance = calculateDistance(observerHeight, turbineHeight);
+      document.getElementById('result').innerHTML = `<p>The turbine can be seen from up to ${(distance / 1000).toFixed(1)} kilometres away in perfect conditions.</p>`;
+    } else {
+      document.getElementById('result').innerHTML = '<p>Enter valid numbers</p>';
+    }
+  });
+  
+  function calculateDistance(observerHeight, turbineHeight) {
+    const radiusEarth = 6371000; // Approximate radius of the Earth in kilometers
+    return Math.sqrt((radiusEarth + observerHeight) ** 2 - radiusEarth ** 2) +
+        Math.sqrt((radiusEarth + turbineHeight) ** 2 - radiusEarth ** 2);
+  }
+  
